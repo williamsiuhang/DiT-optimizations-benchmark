5935b9c5c2a4ea45aecdb87d91100d05eded1fc3
William Siuhang <williamsiuhang@gmail.com>
2025-04-24 01:12:55 -0400

    Use .env target volume on setup1 script

--- scripts/setup/setup1.sh
+++ scripts/setup/setup1.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
-# Install rsync manually on instance first
-# apt update && apt install -y rsync
-
 set -e
 
+# Install rsync manually on instance first
+# apt update && apt install -y rsync
+# Load variables from .env file in the current directory
+if [ -f .env ]; then
+    export $(grep -v '^#' .env | xargs)
+else
+    echo ".env file not found in the current directory."
+    exit 1
+fi
+
 echo "=== Step 1: Downloading and Installing Miniconda ==="
-mkdir -p ~/miniconda3
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh # gpu droplet
-# wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-aarch64.sh -O ~/miniconda3/miniconda.sh # macbook M1
-bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
-rm ~/miniconda3/miniconda.sh
+mkdir -p $TARGET_VOLUME/tmp/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $TARGET_VOLUME/tmp/miniconda3/miniconda.sh # gpu droplet
+# wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-aarch64.sh -O $TARGET_VOLUME/tmp/miniconda3/miniconda.sh # macbook M1
+bash $TARGET_VOLUME/tmp/miniconda3/miniconda.sh -b -u -p $TARGET_VOLUME/tmp/miniconda3
+rm $TARGET_VOLUME/tmp/miniconda3/miniconda.sh
 
 # Initialize conda in bash (this adds necessary lines to ~/.bashrc)
-~/miniconda3/bin/conda init bash
+$TARGET_VOLUME/tmp/miniconda3/bin/conda init bash
 . ~/.bashrc
 
 echo "Run 'source ~/.bashrc' manually to initialize the shell environment."
